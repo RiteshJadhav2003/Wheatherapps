35216de3ffdb9baf57a8cf6ca1a0ffd55bb43e61
ritesj <ritesh@gmaile.com>
2025-07-11 16:52:30 +0530

    commited

--- frontend/vite-project/src/components/MessageInput.jsx
+++ frontend/vite-project/src/components/MessageInput.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowRight } from 'lucide-react';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 
 function MessageInput({ input, setInput, onSend, loading }) {
   const handleKeyDown = (e) => {
@@ -17,16 +17,16 @@ function MessageInput({ input, setInput, onSend, loading }) {
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={handleKeyDown}
           placeholder="Ask about the weather..."
-          className="w-full h-20 pl-5 pr-16 py-4 text-base border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 hover:border-blue-400 transition-all duration-200"
+          className="w-full h-20 pl-5 pr-16 py-4 text-base border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-600 hover:border-blue-500 transition-all duration-200"
           disabled={loading}
         />
 
         <button
           onClick={onSend}
           disabled={loading}
-          className="absolute bottom-2 right-2 bg-black text-white px-4 py-2 rounded-full"
+          className="absolute bottom-2 right-2 bg-black text-white w-10 h-10 flex items-center justify-center rounded-md"
         >
-          <ArrowRight size={20} />
+        <ArrowLeft size={20} className="rotate-180" />
         </button>
       </div>
     </div>
